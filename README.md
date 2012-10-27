a663feddd7db3ffb49fbbd396a0138549d6e53d1
Viktor <viktor@kai.local>
2012-10-27 04:34:54 +0200

    Adds the implementation, tests and readme.

--- README.md
+++ README.md
@@ -1,4 +1,114 @@
-jsonrpc2-erlang
-===============
+JSON-RPC 2.0 for Erlang
+=======================
 
-JSON-RPC 2.0 request handler for Erlang
+This is a JSON-RPC 2.0 request handler. It does not handle JSON decoding and
+encoding. This means the JSON decoding and encoding must be handled by the
+caller. It also means that you may use the JSON library of your choice.
+
+Features
+--------
+
+* allows any JSON encoder and decoder, as long as it supports the eep0018 style
+  terms format,
+* no dependencies, except OTP,
+* dispatches parsed requests to a simple callback function
+* supports optional callback map function for batch requests, e.g. to support
+  concurrent processing of the requests in a batch,
+* handles rpc calls and notifications,
+* supports named and unnamed parameters,
+* includes unit tests for all examples in the JSON-RPC 2.0 specification.
+
+Usage
+-----
+
+The processing is done using a callback function. The method and params are passed to the callback
+function and the return value are used as results. Any exception from the callback function is
+caught and results in an appropriate error message.
+
+Example
+-------
+
+``` erlang
+
+my_handler(Method, Params) ->
+    io:format("Got JSON-RPC call to ~p with params ~p~n", [Method, Params]),
+    case Method of
+        <<"foo">> -> lists:reverse(Params);
+        <<"bar">> -> lists:max(Params) + 42
+    end.
+
+example() ->
+    BinJson = <<"{\"jsonroc\": \"2.0\", \"method\": \"foo\", \"params\": [1,2,3], \"id\": 1}">>,
+
+    Response = try jiffy:decode(BinJson) of
+                   DecodedJson ->
+                       jsonrpc2:handle(DecodedJson, fun my_handler/2)
+               catch
+                   _:_ ->
+                       jsonrpc2:parseerror()
+               end,
+
+    case Response of
+        {reply, Reply} ->
+            io:format("Reply ~p~n", [jiffy:encode(Reply)]);
+        noreply ->
+            io:format("No reply~n")
+    end.
+```
+
+Concurrent Batch Requests
+-------------------------
+
+Using jsonrpc2:handle/2, the requests in a batch request are processesed
+sequentially. To allow concurrent processing, you may use jsonrpc2:handle/3,
+where the 3rd argument should be a function compatible with lists:map/2, such
+as the plists:map/2. You may also want to implement you own map function, to
+use something like a pool of workers.
+
+Error Handling
+--------------
+
+* For JSON parsing errors, jsonrpc2:parseerror() can be called to create a "Parse error" (-32700) response.
+* Invalid JSON-RPC requests (though valid JSON) are reported as "Invalid Request" (-32600).
+* Any exceptions and errors occuring in the handler callback function are caught and reported in the
+  following way:
+  * error:undef  is reported as "Method not found" (-32601),
+  * error:badarg is reported as "Invalid params"
+  * any other error or exception is reported as "    Internal error" (-32603).
+
+The errors undef and badarg normally occur in Erlang when an undefined function is called and when
+a function is not defined for a specific set of parametes, respectively. You can either let them
+occur naturally in your handler or trigger them manually using erlang:error(undef) and
+erlang:error(badarg) respectively.
+
+JSON Data Format
+----------------
+
+    Erlang                          JSON            Erlang
+    ==========================================================================
+
+    null                       -> null           -> null
+    true                       -> true           -> true
+    false                      -> false          -> false
+    "hi"                       -> [104, 105]     -> [104, 105]
+    <<"hi">>                   -> "hi"           -> <<"hi">>
+    hi                         -> "hi"           -> <<"hi">>
+    1                          -> 1              -> 1
+    1.25                       -> 1.25           -> 1.25
+    []                         -> []             -> []
+    [true, 1.0]                -> [true, 1.0]    -> [true, 1.0]
+    {[]}                       -> {}             -> {[]}
+    {[{foo, bar}]}             -> {"foo": "bar"} -> {[{<<"foo">>, <<"bar">>}]}
+    {[{<<"foo">>, <<"bar">>}]} -> {"foo": "bar"} -> {[{<<"foo">>, <<"bar">>}]}
+
+Compatible JSON parsers
+-----------------------
+* Jiffy, https://github.com/davisp/jiffy
+* erlang-json, https://github.com/hio/erlang-json
+* Mochijson2 using ```mochijson2:decode(Bin, [{format, eep18}])```
+
+Links
+-----
+* ejrpc2, another JSON-RPC 2 library, https://github.com/jvliwanag/ejrpc2
+* The JSON-RPC 2.0 specification, http://www.jsonrpc.org/specification
+
---
+++ src/jsonrpc2.erl
@@ -0,0 +1,306 @@
+%% @doc This module handles JSON-RPC 2.0 requests.
+%%
+%% JSON encoding and decoding is not handled by this module. Thus, it must be
+%% handled outside, by the caller.
+%%
+%% The format of the parsed JSON is the so called eep0018 style terms, where
+%% strings are represented as binaries and objects are represented as proplists
+%% wrapped in a single element tuple. This format is supported by several JSON
+%% parsers.
+%%
+%% This work has been released into the public domain by its author.
+-module(jsonrpc2).
+
+-export([handle/2, handle/3, parseerror/0]).
+
+-type json() :: true | false | null | binary() | [json()] | {[{binary(), json()}]}.
+-type method() :: binary().
+-type params() :: [json()] | {[{binary(), json()}]}.
+-type id() :: number() | null | undefined.
+-type request() :: {method(), params(), id()} | invalid.
+-type response() :: {reply, json()} | noreply.
+
+-type handlerfun() :: fun((method(), params()) -> json()).
+-type mapfun() :: fun((fun((A) -> B), [A]) -> [B]). % should be the same as lists:map/2
+
+-export_type([json/0, response/0, handlerfun/0, mapfun/0]).
+
+%% @doc Handles the requests using the handler function. Batch requests are
+%% handled sequentially. Since this module doesn't handle the JSON encoding and
+%% decoding, the request must be JSON decoded before passing it to this
+%% function. Likewise, if a reply is returned, it should be JSON encoded before
+%% sending it to the client.
+-spec handle(json(), handlerfun()) -> response().
+handle(Req, HandlerFun) ->
+    handle(Req, HandlerFun, fun lists:map/2).
+
+%% @doc Handles the requests using the handler function and a custom map
+%% function for batch requests. The map function should be compatible with
+%% lists:map/2. This is useful for concurrent processing of batch requests.
+-spec handle(json(), handlerfun(), mapfun()) -> response().
+handle(Req, HandlerFun, MapFun) ->
+    case parse(Req) of
+        BatchRpc when is_list(BatchRpc), length(BatchRpc) > 0 ->
+            Responses = MapFun(fun(Rpc) -> dispatch(Rpc, HandlerFun) end, BatchRpc),
+            merge_responses(Responses);
+        Rpc ->
+            dispatch(Rpc, HandlerFun)
+    end.
+
+%% @doc Returns a jsonrpc2 parse error response.
+%%
+%% This function can be used to manually create a JSON-SPC error response, for
+%% the case when the client sends invalid JSON. This function is exported for
+%% completeness, since the JSON encoding and decoding is not handled by this
+%% module.
+-spec parseerror() -> json().
+parseerror() ->
+    make_error(-32700, <<"Parse error.">>, null).
+
+%% helpers
+
+-spec make_result_response(json(), id()) -> response().
+make_result_response(_Result, undefined) ->
+    noreply;
+make_result_response(Result, Id) ->
+    {reply, {[{<<"jsonrpc">>, <<"2.0">>}, 
+              {<<"result">>, Result}, 
+              {<<"id">>, Id}]}}.
+
+-spec make_error_response(integer(), binary(), id()) -> response().
+make_error_response(_Code, _Msg, undefined) ->
+    noreply;
+make_error_response(Code, Msg, Id) ->
+    {reply, make_error(Code, Msg, Id)}.
+
+-spec make_error(integer(), binary(), id()) -> json().
+make_error(Code, Msg, Id) ->
+    {[{<<"jsonrpc">>, <<"2.0">>},
+      {<<"error">>, {[{<<"code">>, Code},
+                      {<<"message">>, Msg}]}},
+      {<<"id">>, Id}]}.
+
+%% @doc Parses the RPC part of an already JSON decoded request. Returns a tuple
+%% {Method, Params, Id} for a single request, 'invalid' for an invalid request
+%% and a list of these for a batch request. An Id value of 'undefined' is used
+%% when the id is not present in the request.
+-spec parse(json()) -> request() | [request()].
+parse(Reqs) when is_list(Reqs) ->
+    [parse(Req) || Req <- Reqs];
+parse({Req}) ->
+    Version = proplists:get_value(<<"jsonrpc">>, Req),
+    Method  = proplists:get_value(<<"method">>,  Req),
+    Params  = proplists:get_value(<<"params">>,  Req, []),
+    Id      = proplists:get_value(<<"id">>,      Req, undefined),
+    case Version =:= <<"2.0">>
+           andalso is_binary(Method)
+           andalso (is_list(Params) orelse is_tuple(Params))
+           andalso (Id =:= undefined orelse Id =:= null
+                                     orelse is_binary(Id)
+                                     orelse is_number(Id)) of
+        true ->
+            {Method, Params, Id};
+        false ->
+            invalid
+    end;
+parse(_) ->
+    invalid.
+
+%% @doc Calls the handler function, catches errors and composes a json-rpc response.
+-spec dispatch(request(), handlerfun()) -> response().
+dispatch({Method, Params, Id}, HandlerFun) ->
+    try HandlerFun(Method, Params) of
+        Response -> make_result_response(Response, Id)
+    catch
+        error:undef  -> make_error_response(-32601, <<"Method not found.">>, Id);
+        error:badarg -> make_error_response(-32602, <<"Invalid params.">>, Id);
+        _:_          -> make_error_response(-32603, <<"Internal error.">>, Id)
+    end;
+dispatch(_InvalidReq, _HandlerFun) ->
+    make_error_response(-32600, <<"Invalid Request.">>, null).
+
+%% @doc Transforms a list of responses into a single response.
+-spec merge_responses([response()]) -> response().
+merge_responses(Responses) when is_list(Responses) ->
+    case [Reply || {reply, Reply} <- Responses] of
+        []      -> noreply;
+        Replies -> {reply, Replies}
+    end.
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+%% Testing the examples from http://www.jsonrpc.org/specification
+
+test_handler(<<"subtract">>, [42,23]) -> 19;
+test_handler(<<"subtract">>, [23,42]) -> -19;
+test_handler(<<"subtract">>, {[{<<"subtrahend">>,23},{<<"minuend">>,42}]}) -> 19;
+test_handler(<<"subtract">>, {[{<<"minuend">>,42},{<<"subtrahend">>,23}]}) -> 19;
+test_handler(<<"update">>, [1,2,3,4,5]) -> ok;
+test_handler(<<"foobar">>, _) -> erlang:error(undef);
+test_handler(<<"sum">>, [1,2,4]) -> 7;
+test_handler(<<"foo.get">>, _) -> erlang:error(undef);
+test_handler(<<"get_data">>, []) -> [<<"hello">>,5].
+
+%% rpc call with positional parameters
+  call_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},
+            {<<"method">>,<<"subtract">>},
+            {<<"params">>,[42,23]},
+            {<<"id">>,1}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,19},{<<"id">>,1}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with positional parameters, reverse order
+call2_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},
+            {<<"method">>,<<"subtract">>},
+            {<<"params">>,[23,42]},
+            {<<"id">>,2}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,-19},{<<"id">>,2}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with named parameters
+named_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},
+            {<<"method">>,<<"subtract">>},
+            {<<"params">>,{[{<<"subtrahend">>,23},{<<"minuend">>,42}]}},
+            {<<"id">>,3}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,19},{<<"id">>,3}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with named parameters, reverse order
+named2_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},
+            {<<"method">>,<<"subtract">>},
+            {<<"params">>,{[{<<"minuend">>,42},{<<"subtrahend">>,23}]}},
+            {<<"id">>,4}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,19},{<<"id">>,4}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% a Notification
+notif_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},
+            {<<"method">>,<<"update">>},
+            {<<"params">>,[1,2,3,4,5]}]},
+    noreply = handle(Req, fun test_handler/2).
+
+%% a Notification + non-existent method
+notif2_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},{<<"method">>,<<"foobar">>}]},
+    noreply = handle(Req, fun test_handler/2).
+
+%% rpc call of non-existent method
+bad_method_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},{<<"method">>,<<"foobar">>},{<<"id">>,<<"1">>}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},
+              {<<"error">>,
+               {[{<<"code">>,-32601},{<<"message">>,<<"Method not found.">>}]}},
+              {<<"id">>,<<"1">>}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with invalid JSON
+%% Not applicable, since JSON parsing is not done in this module. We test the error
+%% response though.
+bad_json_test() ->
+    Expected = {[{<<"jsonrpc">>,<<"2.0">>},
+                 {<<"error">>,{[{<<"code">>,-32700},{<<"message">>,<<"Parse error.">>}]}},
+                 {<<"id">>,null}]},
+    Reply = parseerror(),
+    Reply = Expected.
+
+%% rpc call with invalid Request object
+bad_rpc_test() ->
+    Req = {[{<<"jsonrpc">>,<<"2.0">>},{<<"method">>,1},{<<"params">>,<<"bar">>}]},
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},
+              {<<"error">>,{[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+              {<<"id">>,null}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call Batch, invalid JSON:
+%% Not applicable, see bad_json_test/0 above.
+bad_json_batch_test() ->
+    ok.
+
+%% rpc call with an empty Array
+empty_batch_test() ->
+    Req = [],
+    Reply = {[{<<"jsonrpc">>,<<"2.0">>},
+              {<<"error">>,{[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+              {<<"id">>,null}]},
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with an invalid Batch (but not empty)
+invalid_batch_test() ->
+    Req = [1],
+    Reply = [{[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"error">>,
+              {[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+             {<<"id">>,null}]}],
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call with invalid Batch
+invalid_batch2_test() ->
+    Req = [1,2,3],
+    Reply = [{[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"error">>,
+                {[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+               {<<"id">>,null}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"error">>,
+                {[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+               {<<"id">>,null}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"error">>,
+                {[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+               {<<"id">>,null}]}],
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call Batch
+batch_test() ->
+    Req = [{[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"sum">>},
+             {<<"params">>,[1,2,4]},
+             {<<"id">>,<<"1">>}]},
+           {[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"notify_hello">>},
+             {<<"params">>,[7]}]},
+           {[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"subtract">>},
+             {<<"params">>,[42,23]},
+             {<<"id">>,<<"2">>}]},
+           {[{<<"foo">>,<<"boo">>}]},
+           {[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"foo.get">>},
+             {<<"params">>,{[{<<"name">>,<<"myself">>}]}},
+             {<<"id">>,<<"5">>}]},
+           {[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"get_data">>},
+             {<<"id">>,<<"9">>}]}],
+    Reply = [{[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,7},{<<"id">>,<<"1">>}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},{<<"result">>,19},{<<"id">>,<<"2">>}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"error">>,
+                {[{<<"code">>,-32600},{<<"message">>,<<"Invalid Request.">>}]}},
+               {<<"id">>,null}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"error">>,
+                {[{<<"code">>,-32601},{<<"message">>,<<"Method not found.">>}]}},
+               {<<"id">>,<<"5">>}]},
+             {[{<<"jsonrpc">>,<<"2.0">>},
+               {<<"result">>,[<<"hello">>,5]},
+               {<<"id">>,<<"9">>}]}],
+    {reply, Reply} = handle(Req, fun test_handler/2).
+
+%% rpc call Batch (all notifications)
+batch_notif_test() ->
+    Req = [{[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"notify_sum">>},
+             {<<"params">>,[1,2,4]}]},
+           {[{<<"jsonrpc">>,<<"2.0">>},
+             {<<"method">>,<<"notify_hello">>},
+             {<<"params">>,[7]}]}],
+    noreply = handle(Req, fun test_handler/2).
+
+-endif.
